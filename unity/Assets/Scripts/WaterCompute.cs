b4890204a37e1695a9b35b7681227e2c1978cff2
Callum Hay <callumhay@gmail.com>
2020-11-16 17:49:37 -0500

    Got marching cubes and rendering it all working directly on the gpu … pretty limited though, no unity pipeline support :(

--- unity/Assets/Scripts/WaterCompute.cs
+++ unity/Assets/Scripts/WaterCompute.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WaterCompute : MonoBehaviour {
-  public static int waterResolution = 64;
+  public static int waterResolution = 32;
   public static int numThreadsPerBlock = 8;
   public static int numThreadGroups = waterResolution / numThreadsPerBlock;
 
@@ -25,11 +25,18 @@ public class WaterCompute : MonoBehaviour {
 
   private ComputeBuffer isoValuesBuffer;
   private ComputeBuffer meshBuffer;
-  private ComputeBuffer vertCountBuffer;
+  //private ComputeBuffer countBuffer;
 
   // CPU Mesh Rendering
   private MeshFilter meshFilter;
   private MeshRenderer meshRenderer;
+
+  private float timeTracker;
+  private int maxTris;
+  private int maxVerts;
+  //private Bounds bounds;
+
+  private static float unitsPerNode = 0.25f;
 
 
   #pragma warning disable 649 // disable unassigned variable warning
@@ -37,40 +44,39 @@ public class WaterCompute : MonoBehaviour {
     public Vector3 position;
     public Vector3 normal;
   }
-  /*struct Triangle { 
+  struct Triangle { 
     public Vector3 a; 
     public Vector3 b; 
     public Vector3 c;
-  }*/
+  }
 
 
   private void createBuffers() {
     int pointsCount = waterResolution*waterResolution*waterResolution;
     int voxelCount = waterResolution-1;
     voxelCount = voxelCount*voxelCount*voxelCount;
-    int maxTris = voxelCount*5;
-    int maxVerts = maxTris*3;
+    maxTris = voxelCount*5;
+    maxVerts = maxTris*3;
 
     isoValuesBuffer = new ComputeBuffer(pointsCount, sizeof(float));
+    meshBuffer = new ComputeBuffer(maxVerts, sizeof(float)*6);
+    //meshBuffer.SetCounterValue(0);
 
-    meshBuffer = new ComputeBuffer(maxVerts, sizeof(float)*6, ComputeBufferType.Append);
-    meshBuffer.SetCounterValue(0);
-
-    vertCountBuffer = new ComputeBuffer(1, sizeof(int), ComputeBufferType.IndirectArguments);
-
-    //trianglesBuffer = new ComputeBuffer(maxTris, sizeof(float)*9, ComputeBufferType.Append);
-    //trisCountBuffer = new ComputeBuffer(1, sizeof(int), ComputeBufferType.IndirectArguments);
-    //trianglesBuffer.SetCounterValue(0);
-
+    //countBuffer = new ComputeBuffer(1, sizeof(int), ComputeBufferType.IndirectArguments);
     //verticesBuffer = new ComputeBuffer(maxVerts, sizeof(float)*3);
     //normalsBuffer  = new ComputeBuffer(maxVerts, sizeof(float)*3);
     //smoothedNormalsBuffer = new ComputeBuffer(maxVerts, sizeof(float)*3);
+
+    var meshBufSize = maxVerts*6;
+    float[] val = new float[meshBufSize];
+    for (int i = 0; i < meshBufSize; i++) { val[i] = -1.0f; }
+    meshBuffer.SetData(val);
   }
 
   private void removeBuffers() {
     isoValuesBuffer?.Dispose();
     meshBuffer?.Dispose();
-    vertCountBuffer?.Dispose();
+    //countBuffer?.Dispose();
     //trianglesBuffer?.Dispose();
     //trisCountBuffer?.Dispose();
     //verticesBuffer?.Dispose();
@@ -81,7 +87,7 @@ public class WaterCompute : MonoBehaviour {
   private void simulateLiquid() {
     waterCS.SetBuffer(waterCSKernel, "isoValues", isoValuesBuffer);
     waterCS.SetInt("resolutionSize", waterResolution);
-    waterCS.SetFloat("time", Time.timeSinceLevelLoad);
+    waterCS.SetFloat("time", timeTracker);
     //result = new RenderTexture(waterResolution, waterResolution, 0, RenderTextureFormat.ARGBFloat);
     //result.dimension = UnityEngine.Rendering.TextureDimension.Tex3D;
     //result.volumeDepth = waterResolution;
@@ -91,23 +97,23 @@ public class WaterCompute : MonoBehaviour {
     waterCS.Dispatch(waterCSKernel, numThreadGroups, numThreadGroups, numThreadGroups);
   }
   private void generateMesh() {
-    meshBuffer.SetCounterValue(0);
+    //meshBuffer.SetCounterValue(0);
 
-    marchingCubesCS.SetBuffer(mcCSKernel, "vertices", meshBuffer);
+    marchingCubesCS.SetBuffer(mcCSKernel, "meshData", meshBuffer);
     marchingCubesCS.SetBuffer(mcCSKernel, "isoValues", isoValuesBuffer);
     marchingCubesCS.SetInt("numNodesPerSide", waterResolution);
     marchingCubesCS.SetFloat("isoLevel", MarchingCubes.isoValCutoff);
-    marchingCubesCS.SetFloat("unitsPerNode", 0.25f);
+    marchingCubesCS.SetFloat("unitsPerNode", unitsPerNode);
     marchingCubesCS.Dispatch(mcCSKernel, numThreadGroups, numThreadGroups, numThreadGroups);
 
     // Get the number of vertices
-    ComputeBuffer.CopyCount(meshBuffer, vertCountBuffer, 0);
-    int[] countArr = new int[1]{0};
-    vertCountBuffer.GetData(countArr);
-    int vertCount = countArr[0];
+    //ComputeBuffer.CopyCount(meshBuffer, countBuffer, 0);
+    //int[] countArr = new int[1]{0};
+    //countBuffer.GetData(countArr);
+    //int vertCount = countArr[0];
 
     // Turn the triangles into a mesh!
-    var mesh = readMeshGPUToCPU(meshBuffer, vertCount);
+    //var mesh = readMeshGPUToCPUVerts(meshBuffer, maxVerts);
     /*
     // Perform smooth shading on the mesh...
     {
@@ -125,15 +131,17 @@ public class WaterCompute : MonoBehaviour {
       mesh.SetNormals(normals);
     }
     */
-    meshFilter.sharedMesh = mesh;
+    //meshFilter.sharedMesh = mesh;
     
   }
 
   void Start() {
+    timeTracker = 0;
     if (waterCS == null || marchingCubesCS == null || smoothNormalsCS == null) { 
       Debug.LogWarning("One or multiple compute shaders are not set.");
     }
-   
+
+    /*
     meshFilter = GetComponent<MeshFilter>();
     if (!meshFilter) {
       meshFilter = gameObject.AddComponent<MeshFilter>();
@@ -143,9 +151,9 @@ public class WaterCompute : MonoBehaviour {
       meshRenderer = gameObject.AddComponent<MeshRenderer>();
     }
     if (meshRenderer.sharedMaterial == null) {
-      meshRenderer.sharedMaterial = MaterialHelper.defaultMaterial;
+      meshRenderer.sharedMaterial = structBufferMat;//MaterialHelper.defaultMaterial;
     }
-  
+    */
 
     createBuffers();
 
@@ -154,42 +162,46 @@ public class WaterCompute : MonoBehaviour {
     mcCSKernel = marchingCubesCS.FindKernel("CSMarchingCubes");
     smoothNormalsCSKernel = smoothNormalsCS.FindKernel("CSSmoothNormals");
 
-    
-
     //smoothNormalsCS.SetBuffer(smoothNormalsCSKernel, "vertices", verticesBuffer);
     //smoothNormalsCS.SetBuffer(smoothNormalsCSKernel, "normals", normalsBuffer);
     //smoothNormalsCS.SetBuffer(smoothNormalsCSKernel, "smoothedNormals", smoothedNormalsBuffer);
     
-    simulateLiquid();
+    //simulateLiquid();
+    //generateMesh();
   }
 
   void Update() {
-    generateMesh();
-  }
-  void FixedUpdate() {
+    timeTracker += Time.deltaTime * 0.001f;
     simulateLiquid();
+    generateMesh();
+
+    var matBlock = new MaterialPropertyBlock();
+    matBlock.SetBuffer("_Buffer", meshBuffer);
+    //matBlock.SetMatrix("_LocalToWorld", transform.localToWorldMatrix);
+    structBufferMat.SetPass(0);
+    var sizeVec = unitsPerNode*waterResolution * transform.lossyScale;
+    var halfSizeVec = sizeVec/2.0f;
+    var bounds = new Bounds(transform.position + halfSizeVec, sizeVec);
+    Graphics.DrawProcedural(structBufferMat, bounds, MeshTopology.Triangles, maxVerts, 1, null, matBlock, UnityEngine.Rendering.ShadowCastingMode.On, true, gameObject.layer);
+
   }
   /*
   void OnRenderObject() {
+    var matBlock = new MaterialPropertyBlock();
+
     structBufferMat.SetBuffer("_Buffer", meshBuffer);
+    structBufferMat.SetMatrix("_LocalToWorld", transform.localToWorldMatrix);
     structBufferMat.SetPass(0);
-
-    // Get the number of vertices
-    ComputeBuffer.CopyCount(meshBuffer, vertCountBuffer, 0);
-    int[] countArr = new int[1]{0};
-    vertCountBuffer.GetData(countArr);
-    int vertCount = countArr[0];
-
-    Graphics.DrawProceduralNow(MeshTopology.Triangles, vertCount);
+    Graphics.DrawProceduralNow(MeshTopology.Triangles, maxVerts);
   }
   */
+
 
   void OnDestroy() {
     removeBuffers();
   }
 
-
-  private static Mesh readMeshGPUToCPU(in ComputeBuffer vertsBuf, int vertCount) {
+  private static Mesh readMeshGPUToCPUVerts(in ComputeBuffer vertsBuf, int vertCount) {
     Vert[] verts = new Vert[vertCount];
     vertsBuf.GetData(verts, 0, 0, vertCount);
 
@@ -210,6 +222,28 @@ public class WaterCompute : MonoBehaviour {
 
     return mesh;
   }
+  private static Mesh readMeshGPUToCPUTris(in ComputeBuffer trisBuf, int triCount) {
+    Triangle[] tris = new Triangle[triCount];
+    trisBuf.GetData(tris, 0, 0, triCount);
 
+    var mesh = new Mesh();
+    mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+
+    var vertCount = triCount*3;
+    Vector3[] vertices = new Vector3[vertCount];
+    int[] triangles = new int[vertCount];
+
+    for (int i = 0; i < triCount; i++) {
+      var vertIdx = i*3;
+      vertices[vertIdx] = tris[i].a; vertices[vertIdx+1] = tris[i].b; vertices[vertIdx+2] = tris[i].c;
+      triangles[vertIdx] = vertIdx; triangles[vertIdx+1] = vertIdx+1; triangles[vertIdx+2] = vertIdx+2;
+    }
+
+    mesh.SetVertices(vertices);
+    mesh.SetTriangles(triangles, 0);
+    mesh.RecalculateNormals();
+
+    return mesh;
+  }
 
 }
--- unity/Assets/Shaders/DrawVertStructBuffer.shader
+++ unity/Assets/Shaders/DrawVertStructBuffer.shader
@@ -1,43 +1,90 @@
 ﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
 
 Shader "Tactics/Shaders/DrawVertStructBuffer" {
+  Properties {
+      _Color ("Color", Color) = (1,1,1,1)
+      _MainTex ("Albedo (RGB)", 2D) = "white" {}
+      _Glossiness ("Smoothness", Range(0,1)) = 0.5
+      _Metallic ("Metallic", Range(0,1)) = 0.0
+  }
+  
 	SubShader {
-		Pass {
-			Cull back
+  Pass {
+    Tags { "RenderType"="Opaque" }
+		CGPROGRAM
+    
+    //#pragma surface surf Standard vertex:vert addshadow
+    #pragma target 3.5
+    #pragma vertex vert
+    #pragma fragment frag
+    #include "UnityCG.cginc"
 
-			CGPROGRAM
-			#include "UnityCG.cginc"
-			#pragma target 5.0
-			#pragma vertex vert
-			#pragma fragment frag
-			
-			struct Vert {
-				float3 position;
-				float3 normal;
-			};
+    struct Vert {
+      float3 position; 
+      float3 normal;
+    };
+    
+    StructuredBuffer<Vert> _Buffer;
 
-			uniform StructuredBuffer<Vert> _Buffer;
+    struct appInData {
+      uint vid : SV_VertexID;
+    };
 
-			struct v2f {
-				float4 pos : SV_POSITION;
-				float3 col : Color;
-			};
+    struct v2f {
+      float4 vertex : POSITION;
+      float3 normal : NORMAL;
+      float4 tangent : TANGENT;
+      float4 color : COLOR;
+      float4 texcoord : TEXCOORD0;
+      float4 texcoord1 : TEXCOORD1;
+      float4 texcoord2 : TEXCOORD2;
+      float4 texcoord3 : TEXCOORD3;
+      
+    };
+    /*
+    struct Input {
+      float4 color : COLOR;
+      float2 uv_MainTex;
+    };
+    */
 
-			v2f vert(uint id : SV_VertexID) {
-				Vert vert = _Buffer[id];
+    sampler2D _MainTex;
+    half _Glossiness;
+    half _Metallic;
+    fixed4 _Color;
 
-				v2f OUT;
-				OUT.pos = UnityObjectToClipPos(float4(vert.position.xyz, 1));
-				OUT.col = dot(float3(0,1,0), vert.normal) * 0.5 + 0.5;
-			
-				return OUT;
-			}
+    void vert(in appInData input, out v2f v) {
+      //UNITY_INITIALIZE_OUTPUT( Input, o );
 
-			float4 frag(v2f IN) : COLOR {
-				return float4(IN.col,1);
-			}
+      //#if defined(SHADER_API_D3D11) || defined(SHADER_API_METAL) || defined(SHADER_API_GLCORE)
+      Vert currVert = _Buffer[input.vid];
+      v.vertex = UnityObjectToClipPos(float4(currVert.position, 1));
+      v.normal = currVert.normal;  
+      //#else
+      //v.vertex = float4(0,0,0,1);  
+      //v.normal = float3(0,0,0);  
+      //#endif
 
-			ENDCG
+      v.color = float4(1,1,1,1);
+    }
+
+    float4 frag(v2f IN) : COLOR {
+			return IN.color;
 		}
+    /*
+    void surf(Input IN, inout SurfaceOutputStandard o) {
+    
+      // Metallic and smoothness come from slider variables
+      o.Metallic = _Metallic;
+      o.Smoothness = _Glossiness;
+
+      // Albedo comes from a texture tinted by color
+      fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
+      o.Albedo = c.rgb;
+      o.Alpha = c.a;
+    }
+    */
+    ENDCG
 	}
+  }
 }
