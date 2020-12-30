5b2f88f6f9003a6f4cfda3f8882d214b0e802a8e
Callum Hay <callumhay@gmail.com>
2020-12-30 18:25:02 -0500

    Turned cloud rendering into a dome so that it covers the entire sky and horizon.

--- unity/Assets/Scripts/Clouds/CloudVolumeRaymarcher.cs
+++ unity/Assets/Scripts/Clouds/CloudVolumeRaymarcher.cs
@@ -8,32 +8,49 @@ public class CloudVolumeRaymarcher : MonoBehaviour {
   public static readonly float CLOUD_HEIGHT = 200;
   public static readonly float REAL_WORLD_TO_GAME_SCALE = CLOUD_HEIGHT / 1828f;
   public static readonly float DIST_TO_HORIZON = 5000 * REAL_WORLD_TO_GAME_SCALE;
+  public static readonly float CLOUD_CONTAINER_THICKNESS = 500;
 
   public TerrainGrid terrainGrid;
+  public int domeLongitudeSlices = 10;
+  public int domeLatitudeSlices  = 13;
 
   // Private members
   private MeshFilter meshFilter;
   private MeshRenderer meshRenderer;
 
   public void initAll() {
-    // Create the mesh above the battlefield/terrain
-    var aboveContainerSize = new Vector3(5000+2*terrainGrid.xUnitSize(), 500, 5000+2*terrainGrid.zUnitSize());
+    // Create a dome that will hold the clouds - horizon to sky across the panorama
+    float domeHeight = CLOUD_HEIGHT + CLOUD_CONTAINER_THICKNESS;
+    float domeRadius = DIST_TO_HORIZON + CLOUD_CONTAINER_THICKNESS;
+    var tris = new List<int>();
+    var verts = new List<Vector3>();
+    var domeSphereData = MeshHelper.BuildDomeData(domeHeight, domeRadius, domeLongitudeSlices, domeLatitudeSlices, tris, verts);
+
+    var translation = terrainGrid.transform.position + new Vector3(terrainGrid.xUnitSize()/2f, 0, terrainGrid.zUnitSize()/2f);
+    domeSphereData.center += translation;
+    for (int i = 0; i < verts.Count; i++) { verts[i] += translation; }
+
     var mesh = new Mesh();
-    int[] triangles = null;
-    Vector3[] vertices = null;
-    MeshHelper.BuildCubeData(aboveContainerSize, out triangles, out vertices);
-    var translation = terrainGrid.transform.position + new Vector3(-aboveContainerSize.x/2, CLOUD_HEIGHT, -aboveContainerSize.z/2);
-    for (int i = 0; i < vertices.Length; i++) { vertices[i] += translation; }
-    mesh.SetVertices(vertices);
-    mesh.SetTriangles(triangles, 0);
+    mesh.SetVertices(verts);
+    mesh.SetTriangles(tris, 0);
+    mesh.Optimize();
     mesh.RecalculateBounds();
     meshFilter.sharedMesh = mesh;
 
     // Setup the cloud material
     if (meshRenderer.sharedMaterial) {
+
+      var innerRadius = domeSphereData.radius-CLOUD_CONTAINER_THICKNESS;
       var bounds = mesh.bounds;
+      //bounds.max += new Vector3(CLOUD_CONTAINER_THICKNESS,CLOUD_CONTAINER_THICKNESS,CLOUD_CONTAINER_THICKNESS);
+      //bounds.min -= new Vector3(CLOUD_CONTAINER_THICKNESS, 0, CLOUD_CONTAINER_THICKNESS);
+
       meshRenderer.sharedMaterial.SetVector("boundsMax", bounds.max);
       meshRenderer.sharedMaterial.SetVector("boundsMin", bounds.min);
+
+      meshRenderer.sharedMaterial.SetFloat("innerRadius", innerRadius);
+      meshRenderer.sharedMaterial.SetFloat("outerRadius", domeSphereData.radius);
+      meshRenderer.sharedMaterial.SetVector("sphereCenter", domeSphereData.center);
 
       var sunLightGO = GameObject.Find("Directional Light");
       var sunLight = sunLightGO.GetComponent<Light>();
@@ -58,6 +75,10 @@ public class CloudVolumeRaymarcher : MonoBehaviour {
     initAll();
   }
 
+  private void OnValidate() {
+    Invoke("initAll", 0);
+  }
+
   public void setWeather(WeatherData weatherData) {
     var windDir = weatherData.windDirection();
     meshRenderer.sharedMaterial.SetVector("windDir", new Vector3(windDir.x, windDir.y, 0));
@@ -65,6 +86,7 @@ public class CloudVolumeRaymarcher : MonoBehaviour {
     meshRenderer.sharedMaterial.SetFloat("baseSpeed", baseSpeed);
     meshRenderer.sharedMaterial.SetFloat("detailSpeed", 0.5f*baseSpeed);
   }
+
 
 
 }
